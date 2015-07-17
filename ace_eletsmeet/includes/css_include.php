efbc142844c969fde67550b9c50ddda12813b896
sushritcode <sushritcode@users.noreply.github.com>
2015-07-17 15:29:36 +0530

    adding the new ace letsmeet to the repo

---
+++ ace_eletsmeet/includes/css_include.php
@@ -0,0 +1,33 @@
+<!-- bootstrap & fontawesome -->
+<link rel="stylesheet" href="<?php echo CSS_PATH; ?>bootstrap.css" />
+<link rel="stylesheet" href="<?php echo CSS_PATH; ?>font-awesome.css" />
+
+<!-- page specific plugin styles -->
+
+<!-- text fonts -->
+<link rel="stylesheet" href="<?php echo CSS_PATH; ?>ace-fonts.css" />
+
+<!-- ace styles -->
+<link rel="stylesheet" href="<?php echo CSS_PATH; ?>ace.css" class="ace-main-stylesheet" id="main-ace-style" />
+
+<!--[if lte IE 9]>
+        <link rel="stylesheet" href="<?php echo CSS_PATH; ?>ace-part2.css" class="ace-main-stylesheet" />
+<![endif]-->
+
+<link rel="stylesheet" href="<?php echo CSS_PATH; ?>ace-rtl.css" />
+
+<!--[if lte IE 9]>
+  <link rel="stylesheet" href="<?php echo CSS_PATH; ?>ace-ie.css" />
+<![endif]-->
+
+<!-- inline styles related to this page -->
+
+<!-- ace settings handler -->
+<script src="<?php echo JS_PATH; ?>ace-extra.js"></script>
+
+<!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+
+<!--[if lte IE 8]>
+<script src="<?php echo JS_PATH; ?>html5shiv.js"></script>
+<script src="<?php echo JS_PATH; ?>respond.js"></script>
+<![endif]-->
---
+++ ace_eletsmeet/index.php
@@ -0,0 +1,108 @@
+<?php
+require_once('includes/global.inc.php');
+require_once(CLASSES_PATH.'error.inc.php');
+require_once(DBS_PATH.'DataHelper.php');
+require_once(DBS_PATH.'objDataHelper.php');
+//require_once(INCLUDES_PATH.'cm_authfunc.inc.php');
+$CONST_MODULE = 'sample';
+$CONST_PAGEID = 'Sample Page';
+//require_once(INCLUDES_PATH.'cm_authorize.inc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- HEAD CONTENT AREA -->
+        <?php include (INCLUDES_PATH.'head.php'); ?>
+        <!-- HEAD CONTENT AREA -->
+
+         <!-- CSS n JS CONTENT AREA -->
+         <?php include (INCLUDES_PATH.'css_include.php'); ?>    
+         <!-- CSS n JS CONTENT AREA -->
+    </head>
+
+    <body class="no-skin">
+      
+        <!-- TOP NAVIGATION BAR START -->
+        <div id="navbar" class="navbar navbar-default">
+            <?php include (INCLUDES_PATH.'top_navigation.php'); ?>    
+        </div>
+        <!-- TOP NAVIGATION BAR END -->
+        
+         <!-- MAIN CONTAINER START -->
+        <div class="main-container" id="main-container">
+            <script type="text/javascript">
+                try {
+                    ace.settings.check('main-container', 'fixed')
+                } catch (e) {
+                }
+            </script>
+
+            <!-- SIDE NAVIGATION BAR START -->
+            <div id="sidebar" class="sidebar responsive">
+                 <?php include (INCLUDES_PATH.'sidebar_navigation.php'); ?>    
+            </div>
+            <!-- SIDE NAVIGATION BAR END -->
+            
+            <!-- MAIN CONTENT START -->
+            <div class="main-content">
+                <div class="main-content-inner">
+                    
+                    <!-- BREADCRUMBS N SEARCH BAR START -->
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        
+                    </div>
+                    <!-- BREADCRUMBS N SEARCH BAR END -->                    
+                   
+                    <!--  PAGE CONTENT START -->
+                    <div class="page-content">
+                        
+                         <!-- SETTING CONTAINER START -->
+                                  <!--IF NEEDED then WE ADD -->
+                         <!-- SETTING CONTAINER END -->
+                        
+                        <!-- PAGE HEADER -->
+                        <div class="page-header">
+                            <h1>
+                                Dashboard<small><i class="ace-icon fa fa-angle-double-right"></i>overview &amp; stats</small>
+                            </h1>
+                        </div>
+                        <!-- PAGE HEADER -->
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <!-- PAGE CONTENT START -->
+                                
+                               
+
+                                <!-- PAGE CONTENT END -->
+                            </div>
+                        </div> 
+                       
+                    </div>
+                   <!-- PAGE CONTENT END -->
+                    
+                </div>
+            </div>
+            <!--  MAIN CONTENT END -->
+
+            <!-- FOOTER START -->
+            <div class="footer">
+                <?php include (INCLUDES_PATH.'footer.php'); ?>  
+            </div>
+            <!-- FOOTER END -->
+
+            <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+                <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+            </a>
+            
+        </div>
+        <!-- MAIN CONTAINER END -->
+        
+        <!-- JAVA SCRIPT -->
+            <?php include (INCLUDES_PATH.'static_js_includes.php'); ?>  
+            <?php include (INCLUDES_PATH.'other_js_includes.php'); ?>  
+        <!-- JAVA SCRIPT -->
+       
+    </body>
+</html>
