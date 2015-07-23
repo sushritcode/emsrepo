bf27ae02463d47f60c902779dbfb76cf9c09ae64
sushritcode <sushritcode@users.noreply.github.com>
2015-07-23 16:01:50 +0530

    added the function to fetch all the indutry types to common functions

--- ace_eletsmeet/includes/common_function.inc.php
+++ ace_eletsmeet/includes/common_function.inc.php
@@ -30,3 +30,36 @@ function isUserEmailAddressExists($email_address, $dataHelper) {
         throw new Exception("cm_authfunc.inc.php : isUserEmailAddressExists : Could not fetch records : " . $e->getMessage(), 144);
     }
 }
+/* -----------------------------------------------------------------------------
+  Function Name : getAllIndustryType
+  Purpose       : To fetch the list of all the industry types.
+  Parameters    : Datahelper
+  Returns       : List of Companies
+  Calls         : datahelper.fetchRecords
+  Called By     :
+  Author        : Sushrit 
+  Created  on   : 23-July-2015
+  Modified By   :
+  Modified on   :
+  Descriptions  : Table columns are `industry_id`, `industry_name`, `status`
+  ------------------------------------------------------------------------------ */
+function getAllIndustryType($dataHelper)
+{
+	if (!is_object($dataHelper)) 
+	{
+		throw new Exception("common_function.inc.php : isUserEmailAddressExists : DataHelper Object did not instantiate", 104);
+	}
+	try 
+	{
+		$strSqlStatement = "SELECT * FROM industry_details WHERE status like '1';";
+		$arrIndustryTypes = $dataHelper->fetchRecords("QR", $strSqlStatement);
+		return $arrIndustryTypes;
+	} 
+	catch (Exception $e) 
+	{
+		throw new Exception("common_function.inc.php : getAllCompanyType : Could not fetch records : " . $e->getMessage(), 144);
+	}
+
+}
+
+
