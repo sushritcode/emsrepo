de24a7e82179dd83cb28292fd672c3b424055a66
sushritcode <sushritcode@users.noreply.github.com>
2015-07-29 17:28:05 +0530

    api for ajax user profile update

---
+++ ace_eletsmeet/profile/api/action.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('../../includes/global.inc.php');
+require_once(CLASSES_PATH.'error.inc.php');
+require_once(DBS_PATH.'DataHelper.php');
+require_once(DBS_PATH.'objDataHelper.php');
+require_once(INCLUDES_PATH.'cm_authfunc.inc.php');
+require_once(INCLUDES_PATH.'cm_authorize.inc.php');
+require_once(INCLUDES_PATH.'common_function.inc.php');
+require_once(INCLUDES_PATH.'profile_function.inc.php');
+
+
+if(isset($_REQUEST["action"]))
+{
+	switch($_REQUEST["action"])
+	{
+		case "reset":
+			$formMaps  = profile_form_table_map();
+			$updateparams = getUpdateQueryString($_REQUEST , $formMaps);
+			$result  = updateUserProfile($updateparams , $objDataHelper , $strCK_user_id);
+		break;
+	}	
+}
+
+
+
+
+?>
