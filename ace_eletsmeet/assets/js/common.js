6c386a67a5f3de90e0a5d065d82a7e9ca5a1ba26
sushritcode <sushritcode@users.noreply.github.com>
2015-09-10 12:44:53 +0530

    added the javascript function for reset password

--- ace_eletsmeet/assets/js/common.js
+++ ace_eletsmeet/assets/js/common.js
@@ -30,6 +30,58 @@ function getAllElementsValURI(frmName)
 
 	return uri;
 };
+function forgotPwd(frmName,type)
+{
+        document.getElementById("alert").style.display = "none";
+        document.getElementById("succ").style.display = "none";
+        document.getElementById("successmsg").innerHTML="";
+        document.getElementById("err").style.display = "none";
+        document.getElementById("errormsg").innerHTML="";
+        var uri = getAllElementsValURI(document.forms[frmName]);
+        document.getElementById("ajax_loader").style.display = ""; 
+        var frmAction = BASEURL+"profile/api/action.php?action="+type+"&";
+        var url = frmAction+uri;
+        var xmlhttp = initAjax();
+        xmlhttp.onreadystatechange = function() 
+        {
+                if(xmlhttp.readyState==4)
+                {
+
+                        if(xmlhttp.responseText == 1 ) 
+                                showAlert(1,"Please Check your email and follow the steps!!!");
+                        else
+                                showAlert(0,"Please try again , there was some error.");
+                        document.getElementById("ajax_loader").style.display = "none";
+                }
+        };
+        xmlhttp.open("POST",url,true);
+        xmlhttp.send(null);
+        return false;
+};
+function showAlert(type,message)
+{    
+
+        document.getElementById("alert").style.display = "none";
+        document.getElementById("succ").style.display = "none";
+        document.getElementById("successmsg").innerHTML="";
+        document.getElementById("err").style.display = "none";
+        document.getElementById("errormsg").innerHTML="";
+
+        document.getElementById("alert").style.display = ""; 
+        if(type == 1)
+        {
+                document.getElementById("succ").style.display = ""; 
+                document.getElementById("successmsg").innerHTML = message;
+        }
+        else
+        {
+                document.getElementById("err").style.display = "";
+                document.getElementById("errormsg").innerHTML = message;
+        }
+        return true;
+};
+
+
 
 
 
